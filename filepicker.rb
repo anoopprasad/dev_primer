53b27e16c2145db8049dbceedad7edff70d4e35d
Austin Putman, austinfromboston <austin@rawfingertips.com>
2012-12-19 08:29:35 -0800

    offset start must be numeric

--- filepicker.rb
+++ filepicker.rb
@@ -3,7 +3,7 @@ class Filepicker
     current_path = File.expand_path(File.dirname(__FILE__))
     source_path = File.join(current_path, 'sources')
     source_files = Dir.entries(source_path).select { |s| s !~ /^\.\.?$/ }
-    source_file = source_files[Time.now.yday - (ENV['OFFSET_START']||0)]
+    source_file = source_files[Time.now.yday - (ENV['OFFSET_START'].to_i)]
     source_file && File.join(source_path, source_file)
   end
 end
